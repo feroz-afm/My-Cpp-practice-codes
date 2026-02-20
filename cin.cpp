0dc8ee96ccc85487dfd34743f08e5ecf2c9859bc
Md Feroz Ahmed <ferozahmedafm@gmail.com>
2026-02-20 17:26:40 +0600

    add thid code

---
+++ cin.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int a = 40;
+    int b= 59;
+    int sum = a +b ;
+   
+    cout << " The sum of two int is "<< sum <<endl;
+    return 0;
+}
