cd1a7b8a0753541608c0f79a9271a21d3e5bdbd5
Md Feroz Ahmed <ferozahmedafm@gmail.com>
2026-02-20 17:42:04 +0600

    add this code

---
+++ char1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    char str [50];
+    cout<< " Enter the name of your  city :";
+    cin>> str;
+    cout<< " The city is : " << str << endl;
+    return 0;
+}
