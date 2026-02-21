9c110fe3622f7787c3de41b8190dc1c9182decc4
Md Feroz Ahmed <ferozahmedafm@gmail.com>
2026-02-21 23:06:10 +0600

    add this code

---
+++ mulvar.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int y,z,x;
+
+   x=10;
+   y=20;
+   z=30;
+
+   cout<<"value of x: "<<x<<endl<<"value of y: "<<y<<endl<<"value of z: "<<z;
+
+   return 0;
+}
