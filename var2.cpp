e425e64f39c4333e6a2f682315b21380b4cb8110
Md Feroz Ahmed <ferozahmedafm@gmail.com>
2026-02-21 23:12:22 +0600

    add this code

---
+++ var2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int y,z;
+   int x=y=z=10;
+   cout<<"value of x: "<<x<<endl<<"value of y: "<<y<<endl<<"value of z: "<<z;
+
+   return 0;
+}
