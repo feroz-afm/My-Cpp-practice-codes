db4cced17b3d884cb6e6edf80393d7083192422e
Md Feroz Ahmed <ferozahmedafm@gmail.com>
2026-02-21 23:32:35 +0600

    add this code

--- for.cpp
+++ for.cpp
@@ -3,16 +3,18 @@ using namespace std;
 
 int main(){
     int arr[5];
-    int n = 5;
-     
-    cout << "Enter " << n << " array elements: ";
-    for (int i = 0; i < n; i++){
-        // Taking array elements input
+
+    cout << "Enter array elements: ";
+
+    for (int i = 0; i < 5; i++){  
         cin >> arr[i];
     }
 
     cout << "Array elements: ";
-    for (int i = 0; i < n; i++){
+
+    for (int i = 0; i < 5; i++){  
         cout << arr[i] << " ";
     }
+
     return 0;
+}
