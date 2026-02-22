1af2321f544886a513a4ede4309d0090dd2e0b85
Md Feroz Ahmed <ferozahmedafm@gmail.com>
2026-02-22 23:01:48 +0600

    put get 1 char

---
+++ put.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    cout << "Displaying characters: ";
+    cout.put('H');
+    cout.put('e');
+    cout.put('l');
+    cout.put('l');
+    cout.put('o');
+    cout.put(' ');
+    cout.put('C');
+    cout.put('+');
+    cout.put('+');
+    cout << endl;
+    return 0;
+}
