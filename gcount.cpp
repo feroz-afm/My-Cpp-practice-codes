cb45099ad296b04c513abd4c9dff0d1c4f14bab7
Md Feroz Ahmed <ferozahmedafm@gmail.com>
2026-02-22 20:40:13 +0600

    use time measurement code

--- gcount.cpp
+++ gcount.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+#include <chrono>
 using namespace std;
+using namespace std::chrono;
+
 int main() {
+    auto start = high_resolution_clock::now(); // start time
     char buffer[100];
     cout << "Enter text: ";
     cin.getline(buffer, 100);
     cout << "Entered Text: " << buffer << endl;
     cout << "Count of characters: " << cin.gcount() << endl;
+
+    auto stop = high_resolution_clock::now(); // end time
+    auto duration = duration_cast<milliseconds>(stop - start);
+    cout << "Execution Time: " << duration.count() << " ms" << endl;
+
     return 0;
 }
