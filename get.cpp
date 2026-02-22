802e9cd33f04212ee25f781cca64a9316eb40e2e
Md Feroz Ahmed <ferozahmedafm@gmail.com>
2026-02-22 17:41:58 +0600

    add this code

---
+++ get.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "Enter any character: ";
+    char ch = cin.get();
+    cout << "Entered character: '" << ch << "'" << endl;
+    char ch2 = cin.get();
+    cout << "Entered character: '" << ch2 << "'" << endl;
+    return 0;
+}
