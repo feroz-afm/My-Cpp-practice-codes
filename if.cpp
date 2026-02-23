d623ef678e1fa9f9f49b4bc474b85d6ff65759d1
Md Feroz Ahmed <ferozahmedafm@gmail.com>
2026-02-23 23:32:14 +0600

    add this code

---
+++ if.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main ()
+{ 
+    int a=5;
+    if (a>3){
+        cout<< " a is greater than 3"<< endl;
+    }
+    else {
+        cout<< " a is not greater than 3"<< endl;
+    }
+    return 0;
+}
