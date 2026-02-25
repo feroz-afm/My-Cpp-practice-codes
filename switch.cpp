11e2f1c58a7918d0cce2cba21a98358dedeefc71
Md Feroz Ahmed <ferozahmedafm@gmail.com>
2026-02-25 19:46:17 +0600

    add this code

---
+++ switch.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main (){
+    string name = "Feroz";
+
+    if (name == "Feroz") {
+        cout << "My name is Feroz" << endl;
+    }
+    else if (name == "Asmani") {
+        cout << "My name is Asmani" << endl;
+    }
+    else {
+        cout << "You are not my family member" << endl;
+    }
+
+    return 0;
+}
